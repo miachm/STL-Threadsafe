0c5289d2397255b9e73360c6f99022b1bf3a09e3
Miguel Chacon <miachm3@gmail.com>
2017-03-25 20:37:57 +0000

    Refactorized queue.hpp
    Refactorized queue.hpp , removing spanish names and unused variables

--- queue thread safe.hpp
+++ queue thread safe.hpp
@@ -39,49 +39,43 @@ namespace std
 
             queue() {}
             queue(int64_t tam) : lineal_container<T>(tam){}
-            queue(const std::queue<T> &cola) : Cola(cola){}
+            queue(const std::queue<T> &q) : queue(q){}
 
         private:
-            std::queue<T> Cola;
-            std::mutex Cerrojo;
-            std::condition_variable Variable;
-            std::condition_variable Variable_Push;
-            int64_t tam_max = -1;
+            std::queue<T> queue;
 
             bool empty_nothreadsafe() const noexcept override
             {
-                return Cola.empty();
+                return queue.empty();
             }
-
-            // Idem que la anterior pero con un límite de tiempo, lanza excepción si se cumple el tiempo y no ha sido despertado
 
             void pop_nothreadsafe(T& element) override
             {
-                element = Cola.front();
-                Cola.pop();
+                element = queue.front();
+                queue.pop();
             }
 
             void push_nothreadsafe(const T& element) override
             {
-                Cola.push(element);
+                queue.push(element);
             }
 
-            void push_nothreadsafe(T && elemento) override
+            void push_nothreadsafe(T && element) override
             {
-                Cola.push(std::move(elemento));
+                queue.push(std::move(element));
             }
 
             void top_nothreadsafe(T& element) const override
             {
-                element = Cola.front();
+                element = queue.front();
             }
 
             void back_nothreadsafe(T &element) const override
             {
-                element = Cola.back();
+                element = queue.back();
             }
 
-            size_t size_nothreadsafe() const noexcept override {return Cola.size();}
+            size_t size_nothreadsafe() const noexcept override {return queue.size();}
 
         };
     }

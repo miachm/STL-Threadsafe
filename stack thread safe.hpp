543beb25451173fe7ab6d6c609a1027b797aaea1
Miguel Chacon <miachm3@gmail.com>
2017-04-06 13:35:55 +0100

    Renamed stack

--- stack thread safe.hpp
+++ stack thread safe.hpp
@@ -9,34 +9,34 @@ namespace std
         template<class T>
         class stack : public lineal_container<T>
         {
-            std::stack<T> stack;
+            std::stack<T> internal_stack;
 
             void top_nothreadsafe(T &element) const override
             {
-                element = stack.top();
+                element = internal_stack.top();
             }
             void pop_nothreadsafe(T &element) override
             {
-                element = stack.top();
-                stack.pop();
+                element = internal_stack.top();
+                internal_stack.pop();
             }
             void push_nothreadsafe(const T & element) override
             {
-                stack.push(element);
+                internal_stack.push(element);
             }
 
             void push_nothreadsafe(T && element) override
             {
-                stack.push(std::move(element));
+                internal_stack.push(std::move(element));
             }
 
-            bool empty_nothreadsafe() const noexcept override {return stack.empty();}
-            size_t size_nothreadsafe() const noexcept override {return stack.size();}
+            bool empty_nothreadsafe() const noexcept override {return internal_stack.empty();}
+            size_t size_nothreadsafe() const noexcept override {return internal_stack.size();}
 
         public:
             stack(){}
             stack (int64_t e) : lineal_container<T>(e) {}
-            stack(const stack<T> &s) : stack(s) {}
+            stack(const stack<T> &s) : internal_stack(s) {}
         };
     }
 }

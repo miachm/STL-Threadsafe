0717a82a82cb5813836b5c25240039aece5a2454
Miguel Chacon <miachm3@gmail.com>
2017-05-03 23:26:04 +0100

    Small improvements in stack example

--- examples/stack.cpp
+++ examples/stack.cpp
@@ -2,27 +2,29 @@
 #include "stack thread safe.hpp"
 
 constexpr int NUM_WORKERS = 4;
+constexpr int INCR = 3;
 
-void consume(std::threadsafe::stack<int> &stack){
-	for (int i = 0;i < 3;i++){
+void increment(std::threadsafe::stack<int> &stack){
+	for (int i = 0;i < INCR;i++){
 		int n;
 		stack.wait_pop(n);
-		stack.push(n*2);
+		stack.push(n+1);
 	}
 }
 
 int main(){
 	std::threadsafe::stack<int> stack;
-	stack.push(1);
+	stack.push(0);
 
 	std::thread threads[NUM_WORKERS];
 
 	for (int i = 0;i < NUM_WORKERS;i++)
-		threads[i] = std::thread(consume,std::ref(stack));
+		threads[i] = std::thread(increment,std::ref(stack));
 
 	for (int i = 0;i < NUM_WORKERS;i++)
 		threads[i].join();
 
+	// It should print: NUM_WORKERS*INCR
 	int n;
 	stack.wait_pop(n);
 	std::cout << n << std::endl;

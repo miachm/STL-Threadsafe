e67bee17a4b7c44274707d08b35527da840e4875
Miguel <miachm3@gmail.com>
2017-05-10 21:37:02 +0200

    Added testPushThreadSafety in stack

--- tests/teststack.cpp
+++ tests/teststack.cpp
@@ -1,5 +1,7 @@
 #include "stack thread safe.hpp"
 #include "gtest/gtest.h"
+#include <thread>
+#include <algorithm>
 
 TEST(Push,HandleBasicOperation){
 	std::threadsafe::stack<int> stack;
@@ -17,5 +19,67 @@ TEST(Push,HandleBasicOperation){
 			stack.wait_pop(out);
 			EXPECT_EQ(i,out);
 			EXPECT_EQ(i-1,stack.size());
+	}
+}
+
+void wait(std::mutex& lock,std::condition_variable& cond,const bool &ready){
+	std::unique_lock<std::mutex> unique_lock(lock);
+	while (!ready){
+		cond.wait(unique_lock);
+	}
+}
+
+TEST(Push,ThreadSafety){
+	std::threadsafe::stack<int> stack;
+	std::threadsafe::stack<int> another_stack;
+	constexpr int ITERATIONS = 10;
+	std::thread producers[ITERATIONS];
+	std::thread consumers[ITERATIONS];
+	std::mutex lock;
+	std::condition_variable cond;
+	bool ready = false;
+
+	for (int i = 0;i < ITERATIONS;i++){
+		producers[i] = std::thread([&](int id){
+			wait(lock,cond,ready);
+			for (int j = 0;j < ITERATIONS;j++){
+				stack.push(id);
+			}
+		},i);
+	}
+
+	for (int i = 0;i < ITERATIONS;i++){
+		consumers[i] = std::thread([&]{
+			wait(lock,cond,ready);
+			int out;
+			for (int j = 0;j < ITERATIONS;j++){
+				stack.wait_pop(out);
+				ASSERT_LT(out,ITERATIONS);
+				ASSERT_GE(out,0);
+				another_stack.push(out);
+			}
+		});
+	}
+	
+	ready = true;
+	cond.notify_all();
+	for (int i = 0;i < ITERATIONS;i++){
+		producers[i].join();
+		consumers[i].join();
+	}
+
+	ASSERT_EQ(ITERATIONS*ITERATIONS,another_stack.size());
+
+	int freq_table[ITERATIONS];
+	std::fill(freq_table,freq_table+ITERATIONS,0);
+	while (!another_stack.empty()){
+		int out;
+		another_stack.wait_pop(out);
+		freq_table[out]++;
+	}
+
+	for (int i = 0;i < ITERATIONS;i++)
+	{
+		ASSERT_EQ(ITERATIONS,freq_table[i]);
 	}
 }
